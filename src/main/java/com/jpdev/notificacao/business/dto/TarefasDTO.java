150c2c0efc5d9cbed099995841cbd5024fab1bd9
João Paulo Lima <jplopapa.dev@gmail.com>
2026-07-28 22:18:26 -0300

    Adicionado método que envia email

---
+++ src/main/java/com/jpdev/notificacao/business/dto/TarefasDTO.java
@@ -0,0 +1,27 @@
+package com.jpdev.notificacao.business.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jpdev.notificacao.business.enums.StatusNotificacaoEnum;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class TarefasDTO {
+
+    private String id;
+    private String nomeTarefa;
+    private String descricao;
+    private String emailUsuario;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataEvento;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataAlteracao;
+    private StatusNotificacaoEnum statusNotificacaoEnum;
+}
---
+++ src/main/java/com/jpdev/notificacao/business/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,6 @@
+package com.jpdev.notificacao.business.enums;
+
+public enum StatusNotificacaoEnum {
+
+    PENDENTE, NOTIFICADO, CANCELADO
+}
